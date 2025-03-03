a598a7c3ca670531c176b9d5ac4ce83af7f53ead
hvsouza <henriquevieira.souza@gmail.com>
2025-03-03 17:50:17 +0100

    lxplus updates

---
+++ .rootlogon.C
@@ -0,0 +1,10 @@
+{
+  gInterpreter->AddIncludePath("/eos/home-h/hvieirad/WVFAnaSoLAr/class");
+  gInterpreter->AddIncludePath("/eos/home-h/hvieirad/cold_box_analysis/apc_root/cold_box_analysis/class");
+  gStyle->SetPadTickX(1);
+  gStyle->SetPadTickY(1);
+  gStyle->SetPadGridX(1);
+  gStyle->SetPadGridY(1);
+  gStyle->SetOptFit(111);
+}
+
