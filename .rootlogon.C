0df90f3556b2ea7b2e768af6876f12be6ba3a0a7
hvsouza <henriquevieira.souza@gmail.com>
2026-07-27 10:16:28 +0200

    Fixing lxplus history, adding solar to rootlogon, rip old vnc toogle

--- .rootlogon.C
+++ .rootlogon.C
@@ -1,6 +1,8 @@
+#include "TInterpreter.h"
 {
   gInterpreter->AddIncludePath("/eos/home-h/hvieirad/WVFAnaSoLAr/class");
   gInterpreter->AddIncludePath("/eos/home-h/hvieirad/cold_box_analysis/apc_root/cold_box_analysis/class");
+  // gInterpreter->Load("/eos/home-h/hvieirad/SoLAr-decowave/SoLAr-decowave/build/src/libslardec_utils.so");
   gStyle->SetPadTickX(1);
   gStyle->SetPadTickY(1);
   gStyle->SetPadGridX(1);
